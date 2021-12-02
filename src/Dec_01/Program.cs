e2755fa3c35990b13366b7e4e1f7bd410d3df0ed
Cain Voong <cainux@gmail.com>
2021-12-02 21:58:30 +0000

    Initial load

---
+++ src/Dec_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Dec_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            Part01(lines);
+            Part02(lines);
+        }
+
+        static void Part01(string[] lines)
+        {
+            var counter = 0;
+
+            for (var i = 0; i < lines.Length - 1; i++)
+            {
+                var line = int.Parse(lines[i]);
+                var nextLine = int.Parse(lines[i + 1]);
+
+                if (nextLine > line)
+                    counter++;
+            }
+
+            Console.WriteLine($"Part 01 result: {counter}");
+        }
+
+        static void Part02(string[] lines)
+        {
+            var counter = 0;
+
+            for (var i = 0; i < lines.Length - 3; i++)
+            {
+                var batch1 = int.Parse(lines[i]) + int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]);
+                var batch2 = int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]) + int.Parse(lines[i + 3]);
+
+                if (batch2 > batch1)
+                    counter++;
+            }
+
+            Console.WriteLine($"Part 02 result: {counter}");
+        }
+    }
+}
---
+++ src/Dec_02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace Dec_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            Part01(lines);
+            Console.WriteLine();
+            Part02(lines);
+        }
+
+        static void Part01(string[] lines)
+        {
+            var horizontalPosition = 0;
+            var depth = 0;
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(" ");
+                var command = parts[0];
+                var value = int.Parse(parts[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        horizontalPosition += value;
+                        break;
+
+                    case "up":
+                        depth -= value;
+                        break;
+
+                    case "down":
+                        depth += value;
+                        break;
+
+                    default:
+                        throw new Exception($"Unrecognised Command: {command}");
+                }
+            }
+
+            Console.WriteLine("Part 01 Results");
+            Console.WriteLine($"Horizontal Position: {horizontalPosition}, Depth: {depth}");
+            Console.WriteLine($"Result: {horizontalPosition * depth}");
+        }
+
+        static void Part02(string[] lines)
+        {
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(" ");
+                var command = parts[0];
+                var value = int.Parse(parts[1]);
+
+                switch (command)
+                {
+                    case "forward":
+                        horizontalPosition += value;
+                        depth += aim * value;
+                        break;
+
+                    case "up":
+                        aim -= value;
+                        break;
+
+                    case "down":
+                        aim += value;
+                        break;
+
+                    default:
+                        throw new Exception($"Unrecognised Command: {command}");
+                }
+            }
+
+            Console.WriteLine("Part 02 Results");
+            Console.WriteLine($"Horizontal Position: {horizontalPosition}, Depth: {depth}");
+            Console.WriteLine($"Result: {horizontalPosition * depth}");
+        }
+    }
+}
