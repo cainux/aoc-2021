1231c1e1a59b3e927d624b52e01d6f793538f23e
Cain Voong <cainux@gmail.com>
2021-12-03 21:45:46 +0000

    Day 3 Part 2

--- src/Dec_03/Program.cs
+++ src/Dec_03/Program.cs
@@ -12,6 +12,7 @@ namespace Dec_03
 
             Part01(lines);
             Console.WriteLine();
+            Part02(lines);
         }
 
         static void Part01(string[] lines)
@@ -36,6 +37,50 @@ namespace Dec_03
             Console.WriteLine("Part 01 Results");
             Console.WriteLine($"Gamma: {gamma}, Epsilon: {epsilon}");
             Console.WriteLine($"Result: {gamma * epsilon}");
+        }
+
+        static void Part02(string[] lines)
+        {
+            var oxygenRating = OxygenRating(0, lines);
+            var co2ScrubberRating = CO2ScrubberRating(0, lines);
+
+            Console.WriteLine("Part 02 Results");
+            Console.WriteLine($"Oxygen Rating: {oxygenRating}, CO2 Scrubber Rating: {co2ScrubberRating}");
+            Console.WriteLine($"Result: {oxygenRating * co2ScrubberRating}");
+        }
+
+        static int OxygenRating(int position, string[] lines)
+        {
+            var winningBit = lines
+                .GroupBy(x => x[position])
+                .Select(x => new {Bit = x.Key, Count = x.Count()})
+                .OrderByDescending(x => x.Count)
+                .ThenByDescending(x => x.Bit)
+                .Select(x => x.Bit)
+                .First();
+
+            var survivingLines = lines
+                .Where(x => x[position] == winningBit)
+                .ToArray();
+
+            return survivingLines.Length > 1 ? OxygenRating(position + 1, survivingLines) : Convert.ToInt32(survivingLines[0], 2);
+        }
+
+        static int CO2ScrubberRating(int position, string[] lines)
+        {
+            var winningBit = lines
+                .GroupBy(x => x[position])
+                .Select(x => new {Bit = x.Key, Count = x.Count()})
+                .OrderBy(x => x.Count)
+                .ThenBy(x => x.Bit)
+                .Select(x => x.Bit)
+                .First();
+
+            var survivingLines = lines
+                .Where(x => x[position] == winningBit)
+                .ToArray();
+
+            return survivingLines.Length > 1 ? CO2ScrubberRating(position + 1, survivingLines) : Convert.ToInt32(survivingLines[0], 2);
         }
     }
 }
