695695cb5c03f890b0024bcf3904973a33e05287
Cain Voong <cainux@gmail.com>
2021-12-10 16:47:26 +0000

    Day 8 pt 2

--- src/Dec_08/Program.cs
+++ src/Dec_08/Program.cs
@@ -10,6 +10,8 @@ foreach (var line in lines)
 }
 
 Part01(input); // Test: 26
+Console.WriteLine();
+Part02(input); // Test: 61229
 
 void Part01(List<(string[], string[])> input)
 {
@@ -43,4 +45,74 @@ void Part01(List<(string[], string[])> input)
     }
 
     Console.WriteLine($"Part 1 result: {result}");
+}
+
+void Part02(List<(string[], string[])> input)
+{
+    var result = 0;
+
+    foreach (var (signal, output) in input)
+    {
+        var orderedPatterns = signal
+            .Select(x => string.Join(string.Empty, x.OrderBy(y => y)))
+            .ToArray();
+
+        // 🤮
+        var one = orderedPatterns.Single(x => x.Length == 2);
+        var four = orderedPatterns.Single(x => x.Length == 4);
+        var seven = orderedPatterns.Single(x => x.Length == 3);
+        var eight = orderedPatterns.Single(x => x.Length == 7);
+        var top = seven.Minus(one);
+        var nine = orderedPatterns.Single(x => x.Length == 6 && x.Has(four.Combine(top)));
+        var bottom = nine.Minus(four.Combine(top));
+        var bottomLeft = eight.Minus(nine);
+        var two = orderedPatterns.Single(x => x.Length == 5 && x.Combine(bottomLeft) == x);
+        var topLeft = nine.Minus(two).Minus(one).Minus(bottom);
+        var zero = seven.Combine(topLeft).Combine(bottomLeft).Combine(bottom);
+        var bottomRight = zero.Minus(two).Minus(topLeft);
+        var topRight = one.Minus(bottomRight);
+        var three = eight.Minus(topLeft).Minus(bottomLeft);
+        var six = eight.Minus(topRight);
+        var five = six.Minus(bottomLeft);
+
+        var decoder = new Dictionary<string, char>
+        {
+            { zero, '0' },
+            { one, '1' },
+            { two, '2' },
+            { three, '3' },
+            { four, '4' },
+            { five, '5' },
+            { six, '6' },
+            { seven, '7' },
+            { eight, '8' },
+            { nine, '9' }
+        };
+
+        var outputValue = output
+            .Select(x => string.Join(string.Empty, x.OrderBy(y => y)))
+            .Aggregate(string.Empty, (current, o) => current + decoder[o]);
+
+        result += Convert.ToInt32(outputValue);
+    }
+
+    Console.WriteLine($"Part 2 result: {result}");
+}
+
+static class Extensions
+{
+    public static string Combine(this string x, string y)
+    {
+        return string.Join(string.Empty, (x + y).OrderBy(c => c).Distinct());
+    }
+
+    public static string Minus(this string x, string y)
+    {
+        return y.Aggregate(x, (current, c) => current.Replace(c.ToString(), ""));
+    }
+
+    public static bool Has(this string x, string y)
+    {
+        return y.All(x.Contains);
+    }
 }
