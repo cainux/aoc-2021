70177e190a49fe7c1c6591b582662df3445f8c5e
Cain Voong <cainux@gmail.com>
2021-12-14 16:51:56 +0000

    Day 12 pt2

--- src/Dec_12/Program.cs
+++ src/Dec_12/Program.cs
@@ -1,4 +1,4 @@
-﻿var lines = File.ReadLines("input.txt");
+﻿var lines = File.ReadLines("input-test.txt");
 var paths = new List<Path>();
 
 foreach (var line in lines)
@@ -8,30 +8,56 @@ foreach (var line in lines)
 }
 
 Part01(paths); // Test: 226
+Part02(paths); // Test: 3509
 
 static void Part01(List<Path> area)
 {
     var result = new List<string>();
 
-    var points = area.Where(x => x.C1 == "start" || x.C2 == "start").ToArray();
+    var starts = area.Where(x => x.C1 == "start" || x.C2 == "start").ToArray();
 
-    foreach (var (c1, c2) in points)
+    foreach (var (c1, c2) in starts)
     {
         var currentRoute = c1 == "start" ? $"{c1},{c2}" : $"{c2},{c1}";
-        var paths = GetPaths(currentRoute, c1 == "start" ? c2 : c1, area);
+        var paths = FindPaths(currentRoute, c1 == "start" ? c2 : c1, area);
         result.AddRange(paths);
-    }
-
-    foreach (var route in result)
-    {
-        Console.WriteLine(route);
     }
 
     Console.WriteLine();
     Console.WriteLine($"Part 1 result: {result.Count} paths");
 }
 
-static IEnumerable<string> GetPaths(string currentRoute, string location, List<Path> area)
+static void Part02(List<Path> area)
+{
+    var result = new List<string>();
+
+    var starts = area.Where(x => x.C1 == "start" || x.C2 == "start").ToArray();
+
+    var smallCaves = area
+        .SelectMany(x => new[] {x.C1, x.C2})
+        .Where(x => x != "start" && x != "end")
+        .Where(x => x.ToLower() == x)
+        .Distinct()
+        .OrderBy(x => x)
+        .ToArray();
+
+    foreach (var smallCave in smallCaves)
+    {
+        foreach (var (c1, c2) in starts)
+        {
+            var currentRoute = c1 == "start" ? $"{c1},{c2}" : $"{c2},{c1}";
+            var paths = FindPaths(currentRoute, c1 == "start" ? c2 : c1, area, smallCave);
+            result.AddRange(paths);
+        }
+    }
+
+    result = result.Distinct().ToList();
+
+    Console.WriteLine();
+    Console.WriteLine($"Part 2 result: {result.Count} paths");
+}
+
+static IEnumerable<string> FindPaths(string currentRoute, string location, List<Path> area, string specialSmallCave = "")
 {
     var result = new List<string>();
 
@@ -44,6 +70,16 @@ static IEnumerable<string> GetPaths(string currentRoute, string location, List<P
     var visitedSmallCaves = currentRoute.Split(',')
         .Where(x => x.ToLower() == x)
         .ToArray();
+
+    if (specialSmallCave != string.Empty)
+    {
+        if (visitedSmallCaves.Count(x => x == specialSmallCave) < 2)
+        {
+            visitedSmallCaves = visitedSmallCaves
+                .Where(x => x != specialSmallCave)
+                .ToArray();
+        }
+    }
 
     var connections = area
         // Paths which involve location
@@ -60,7 +96,7 @@ static IEnumerable<string> GetPaths(string currentRoute, string location, List<P
 
     foreach (var connection in connections)
     {
-        var paths = GetPaths($"{currentRoute},{connection}", connection, area);
+        var paths = FindPaths($"{currentRoute},{connection}", connection, area, specialSmallCave);
         result.AddRange(paths);
     }
 
