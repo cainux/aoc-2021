f1a5f1dfd6ace0ba44840fa4d69d6a47e6a02800
Cain Voong <cainux@gmail.com>
2021-12-19 01:25:55 +0000

    Day 14 part 2, which I really hate

--- src/Dec_14/Program.cs
+++ src/Dec_14/Program.cs
@@ -1,49 +1,49 @@
-﻿using System.Text;
-
-var lines = File.ReadLines("input-test.txt").ToArray();
+﻿var lines = File.ReadLines("input-test.txt").ToArray();
 var template = lines[0];
 var rules = lines[2..].Select(line => line.Split(" -> ")).ToDictionary(rule => rule[0], rule => rule[1][0]);
 
-Run(template, rules); // Test: 1588 (10 steps), 2188189693529 (40 steps)
+Run(template, rules, 40); // Test: 1588 (10 steps), 2188189693529 (40 steps)
 
-static void Run(string template, Dictionary<string, char> rules, int steps = 10)
+static void Run(string template, IReadOnlyDictionary<string, char> rules, int steps)
 {
-    var result = template;
+    var pairs = new Dictionary<string, ulong>();
+    var letters = new Dictionary<char, ulong>();
 
-    for (var i = 1; i <= steps; i++)
-    {
-        result = Polymerize(result, rules);
-    }
-
-    var rankings = result.GroupBy(x => x).Select(x => new {x.Key, x.ToList().Count}).ToArray();
-    var top = rankings.Max(x => x.Count);
-    var bottom = rankings.Min(x => x.Count);
-
-    Console.WriteLine($"Result after {steps} steps: {top - bottom}");
-}
-
-static string Polymerize(string template, Dictionary<string, char> rules)
-{
-    var sb = new StringBuilder();
+    foreach (var c in template)
+        letters.AddCrement(c);
 
     for (var i = 0; i < template.Length - 1; i++)
+        pairs.AddCrement($"{template[i]}{template[i + 1]}");
+
+    for (var step = 1; step <= steps; step++)
     {
-        var pair = $"{template[i]}{template[i + 1]}";
+        var nextStep = new Dictionary<string, ulong>();
 
-        if (rules.ContainsKey(pair))
+        foreach (var (pair, count) in pairs.Where(x => x.Value > 0))
         {
-            sb.Append($"{template[i]}{rules[pair]}");
-        }
-        else
-        {
-            sb.Append(pair);
+            if (!rules.ContainsKey(pair)) continue;
+
+            var insert = rules[pair];
+
+            letters.AddCrement(insert, count);
+
+            foreach (var newPair in new[] { $"{pair[0]}{insert}", $"{insert}{pair[1]}" })
+                nextStep.AddCrement(newPair, count);
         }
 
-        if (i + 2 == template.Length)
-        {
-            sb.Append(template[i + 1]);
-        }
+        pairs = nextStep;
     }
 
-    return sb.ToString();
+    Console.WriteLine($"Result after {steps} steps: {letters.Max(x => x.Value) - letters.Min(x => x.Value)}");
+}
+
+internal static class Extensions
+{
+    public static void AddCrement<TKey>(this Dictionary<TKey, ulong> dictionary, TKey key, ulong increment = 1) where TKey : notnull
+    {
+        if (dictionary.ContainsKey(key))
+            dictionary[key] += increment;
+        else
+            dictionary.Add(key, increment);
+    }
 }
